0dc99455f8971a5bdfaada722d8dfe355ed5178d
pronom8 <mangros19@gmail.com>
2026-01-29 16:24:27 +0200

    made the code look a bit nicer

--- part2/tdd-mooc-tetris/src/RotatingShape.ts
+++ part2/tdd-mooc-tetris/src/RotatingShape.ts
@@ -1,10 +1,16 @@
 export class RotatingShape{
     base;
-    constructor(){this.base = ""   }
+    constructor(){
+        this.base = ""   
+    }
     static fromString(stringi:string):RotatingShape{
-        const linet = stringi.split("\n").map(line => line.trim()).filter(line=>line!="")
+        const linet = stringi.split("\n")
+        .map(line => line.trim()).filter(line=>line!="")
         const shape = new RotatingShape()
+
         shape.base = linet.join("\n")+"\n"
-        return shape  }
+        return shape  
+    }
     toString(){
-        return this.base    }}
+        return this.base    
+    }}
