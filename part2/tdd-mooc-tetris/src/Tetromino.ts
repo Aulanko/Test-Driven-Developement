ed30069b4a82f43f20e17411e21e21019a601d78
pronom8 <mangros19@gmail.com>
2026-02-03 17:21:11 +0200

    a lil fix

--- part2/tdd-mooc-tetris/src/Tetromino.ts
+++ part2/tdd-mooc-tetris/src/Tetromino.ts
@@ -38,9 +38,9 @@ export class Tetromino{
       rotateLeft(){
         const positions =[
             ['.T.','TTT','...'],
-            ['.T.','.TT','.T.'],
+            ['.T.','TT.','.T.'],
             ['...','TTT','.T.'],
-            ['.T.','TT.','.T.']
+            ['.T.','.TT','.T.']
         ]
         this.position =(this.position+3)%4
         this.base=positions[this.position]
