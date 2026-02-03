0817fcf6b76aa0332b64c8fe9e4bff82ef864e21
pronom8 <mangros19@gmail.com>
2026-02-03 16:46:37 +0200

    implemented an initial rotateRight functionality

--- part2/tdd-mooc-tetris/src/Tetromino.ts
+++ part2/tdd-mooc-tetris/src/Tetromino.ts
@@ -20,5 +20,13 @@ export class Tetromino{
         return shape
     }
 
- 
-}
+    rotateRight(){
+        let counter = 0
+        let final_list = []
+        for(let i =0; i<this.base.length;i++){
+            let new_list = ""
+            for(let j of this.base){
+                new_list = j[counter] + new_list}  final_list.push(new_list)
+        } counter ++;
+        this.base = final_list
+        return this }}
