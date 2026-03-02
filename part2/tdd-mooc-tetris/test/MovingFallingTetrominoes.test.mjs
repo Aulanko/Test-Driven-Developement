16ad04c082036182860b187511e1ceddb1e5a37a
pronom8 <mangros19@gmail.com>
2026-03-02 17:20:18 +0200

    adding initials to MovingFallintetrominoes

---
+++ part2/tdd-mooc-tetris/test/MovingFallingTetrominoes.test.mjs
@@ -0,0 +1,10 @@
+
+import { Board } from "../src/Board";
+import { Tetromino } from "../src/Tetromino";
+import { expect } from "chai";
+
+function fallToBottom(board) {
+  for (let i = 0; i < 10; i++) {
+    board.tick();
+  }
+}
