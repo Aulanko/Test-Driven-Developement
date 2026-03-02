330b107c15b94969fd22eb40efda055afcef504e
pronom8 <mangros19@gmail.com>
2026-03-02 15:30:15 +0200

    initializing the falling block shape

--- part2/tdd-mooc-tetris/src/Board.ts
+++ part2/tdd-mooc-tetris/src/Board.ts
@@ -95,7 +95,11 @@ export class Board {
     const blocksFalling = []
     for(let row = 0; row<this.finalBlocks.length;row++){
       for(let col = 0; col<this.finalBlocks.length;col++){
-        
+        if(this.finalBlocks[row][col]!=""){
+          const fallingRow = this.blocki.y+row
+          const fallingCol = this.blocki.x+col
+          blocksFalling.push({row:fallingRow, col:fallingCol})
+        }
       }
     }
     
