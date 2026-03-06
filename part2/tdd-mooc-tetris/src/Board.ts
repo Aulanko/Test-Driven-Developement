e572a41213a2010f2cf8e6152a590bef254ea58a
pronom8 <mangros19@gmail.com>
2026-03-06 17:44:28 +0200

    adding empty space filtering

--- part2/tdd-mooc-tetris/src/Board.ts
+++ part2/tdd-mooc-tetris/src/Board.ts
@@ -187,8 +187,16 @@ export class Board {
 
   shapeTrim(blocks:string[]){
     const nonEmptyRows = blocks.filter(row => row.includes('T') || row.includes('I') || row.includes('O'));
-    
-    return 
+
+    let minCol= blocks[0].length;
+    let maxCol= -1;
+    for (const row of nonEmptyRows) {
+        for (let col = 0;col<row.length; col++) {
+            if (row[col]!=='.') {
+                minCol = Math.min(minCol, col);
+                maxCol = Math.max(maxCol, col);     }
+        }   }
+    return nonEmptyRows.map(row => row.substring(minCol, maxCol + 1));
   }
 
 
